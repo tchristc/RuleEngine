652549fe69ed3e0a27f83b38138250ead2db63ab
tchristc <tom.c.christensen@gmail.com>
2017-03-19 13:06:40 -0500

    filter

---
+++ RuleEngine/Filter.cs
@@ -0,0 +1,9 @@
+﻿namespace RuleEngine
+{
+    public class Filter
+    {
+        public string RightHand { get; set; }
+        public string LeftHand { get; set; }
+        public string Operation { get; set; }
+    }
+}
